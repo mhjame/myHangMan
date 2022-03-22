8ec8893ce80f40774a040352bfd74328e3792568
Doan Thi Minh Hang <doanthiminhhangit@gmail.com>
2022-03-22 13:45:05 +0700

    version 1.0

---
+++ hangMan.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const string WORD_LIST[] = {"dog", "cat", "human"};
+const int WORD_COUNT = sizeof(WORD_LIST)/sizeof(string);
+const string FIGURE[] = {"   -------------     \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         " -----               \n",
+
+                         "   -------------     \n"
+                         "   |           |     \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         " -----               \n",
+
+                         "   -------------     \n"
+                         "   |           |     \n"
+                         "   |           0     \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         " -----               \n",
+
+                         "   -------------     \n"
+                         "   |           |     \n"
+                         "   |           0     \n"
+                         "   |           |     \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         " -----               \n",
+
+                         "   -------------     \n"
+                         "   |           |     \n"
+                         "   |           0     \n"
+                         "   |          /|     \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         " -----               \n",
+
+                         "   -------------     \n"
+                         "   |           |     \n"
+                         "   |           0     \n"
+                         "   |          /|\\     \n"
+                         "   |                 \n"
+                         "   |                 \n"
+                         " -----               \n",
+
+                         "   -------------     \n"
+                         "   |           |     \n"
+                         "   |           0     \n"
+                         "   |          /|\\     \n"
+                         "   |          /      \n"
+                         "   |                 \n"
+                         " -----               \n",
+
+                          "   -------------     \n"
+                         "   |           |     \n"
+                         "   |           0     \n"
+                         "   |          /|\\     \n"
+                         "   |          / \\     \n"
+                         "   |                 \n"
+                         " -----               \n"};
+
+string chooseWord()
+{
+    int randomIndex = rand()%WORD_COUNT;
+    return WORD_LIST[randomIndex];
+}
+
+void renderGame(string guessedWord, int badGuessCount)
+{
+    cout << FIGURE[badGuessCount] << endl;
+    cout << guessedWord;
+    cout << "Number of wrong guesses: " << badGuessCount << endl;
+}
+
+string update(string guessedWord, string secretWord, char guess)
+{
+    for(int i = secretWord.length() - 1; i >= 0; --i)
+    {
+        if(secretWord[i] == guess)
+        {
+            guessedWord[i] = guess;
+        }
+    }
+    return guessedWord;
+}
+
+char readAGuess()
+{
+    char guess;
+    cin >> guess;
+    return guess;
+}
+
+bool contains(string secretWord, char guess)
+{
+    if(secretWord.find_first_of(guess) != string::npos) return true;
+    return false;
+}
+
+string secretWord = chooseWord();
+string guessedWord = string(secretWord.length(), '-');
+
+int badGuessCount = 0;
+const int maxBadGuesses = 7;
+
+int main()
+{
+    do
+    {
+        renderGame(guessedWord, badGuessCount);
+        char guess = readAGuess();
+        if(contains(secretWord, guess))
+        {
+            guessedWord = update(guessedWord, secretWord, guess);
+        }
+        else badGuessCount++;
+    }while(badGuessCount < maxBadGuesses && secretWord != guessedWord);
+
+    renderGame(guessedWord, badGuessCount);
+    if(badGuessCount < maxBadGuesses) cout << "congratulation! You win!";
+    else cout << "You lost. The correct word is " << secretWord;
+
+    return 0;
+}
